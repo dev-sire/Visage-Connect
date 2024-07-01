079285279fe2cee1fc2db1328d42dfac5f452bc4
Aman Shahid <shahidamaan2019@gmail.com>
2024-07-01 21:26:16 +0500

    final fix

--- convex/requests.ts
+++ convex/requests.ts
@@ -32,22 +32,29 @@ export const get = query({ args: {}, handler: async (ctx, args) => {
     return requestWithSender;
 }})
 
-export const count = query({ args: {}, handler: async(ctx, args) => {
-    const identity = await ctx.auth.getUserIdentity()
-
-    if(!identity){
-        throw new Error("Unauthorized!")
-    }
-
-    const currentUser = await getUserByClerkId({
-        ctx, clerkId: identity.subject
-    })
-
-    if(!currentUser){
-        throw new ConvexError("User could not be found")
-    }
-
-    const requests = await ctx.db.query("requests").withIndex("by_receiver", (q) => q.eq("receiver", currentUser._id)).collect()
-
-    return requests.length;
-}})
+export const count = query({
+    args: {},
+    handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      if (!identity) {
+        throw new Error("Unauthorized");
+      }
+  
+      const currentUser = await getUserByClerkId({
+        ctx,
+        clerkId: identity.subject,
+      });
+  
+      if (!currentUser) {
+        throw new ConvexError("User not found");
+      }
+  
+      const requests = await ctx.db
+        .query("requests")
+        .withIndex("by_receiver", (q) => q.eq("receiver", currentUser._id))
+        .collect();
+  
+      return requests.length;
+    },
+});
